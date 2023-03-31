8275082460b2b30f7103da4407c9f181e1bcc970
Alief Ahmad Azies <alief.ahmadazies@gmail.com>
2023-03-31 12:44:09 +0700

    finished Genre UI

---
+++ Bookist/Module/Genre/Networking/APIService.swift
@@ -0,0 +1,37 @@
+//
+//  APIService.swift
+//  Bookist
+//
+//  Created by Alief Ahmad Azies on 31/03/23.
+//
+
+import Foundation
+
+protocol ApiServiceProtocol {
+    mutating func get(url: URL)
+    func callApi<T: Codable>(model: T.Type, completion: @escaping (Result<T, Error>) -> Void)
+}
+
+struct ApiService: ApiServiceProtocol {
+    
+    private var url = URL(string: "")
+    
+    mutating func get(url: URL) {
+        self.url = url
+    }
+    
+    func callApi<T>(model: T.Type, completion: @escaping (Result<T, Error>) -> Void) where T : Decodable, T : Encodable {
+        guard let url = self.url else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                do {
+                    let photosData = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(photosData))
+                } catch let error {
+                    completion(.failure(error))
+                }
+            }
+        }.resume()
+    }
+}
