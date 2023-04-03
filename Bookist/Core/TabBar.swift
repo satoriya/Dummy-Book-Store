2038d77f80f8817ac13944f4a96042113dd5a60a
farhanpermana <46885640+farhanpermana@users.noreply.github.com>
2023-04-04 05:10:23 +0700

    Update TabBar.swift

--- Bookist/Core/TabBar.swift
+++ Bookist/Core/TabBar.swift
@@ -21,7 +21,7 @@ class TabBar: UITabBarController {
                createNavController(for: ViewController(), title: NSLocalizedString("Home", comment: ""), image: UIImage(systemName: "magnifyingglass")!),
                createNavController(for: DiscoverViewController(), title: NSLocalizedString("Discover", comment: ""), image: UIImage(systemName: "house")!),
                createNavController(for: WishlistViewController(), title: NSLocalizedString("Wishlist", comment: ""), image: UIImage(systemName: "person")!),
-               createNavController(for: ViewController(), title: NSLocalizedString("Purchased", comment: ""), image: UIImage(systemName: "person")!),
+               createNavController(for: PurchasedViewController(), title: NSLocalizedString("Purchased", comment: ""), image: UIImage(systemName: "person")!),
                createNavController(for: AccountViewController(), title: NSLocalizedString("Account", comment: ""), image: UIImage(systemName: "person")!)
            ]
        }
