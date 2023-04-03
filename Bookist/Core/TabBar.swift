5b518297e2c0ced720a6be94282b0caa5370dbcb
fathyaa <fathyariyani@gmail.com>
2023-04-03 12:28:35 +0700

    add account page

--- Bookist/Core/TabBar.swift
+++ Bookist/Core/TabBar.swift
@@ -22,7 +22,7 @@ class TabBar: UITabBarController {
                createNavController(for: ViewController(), title: NSLocalizedString("Discover", comment: ""), image: UIImage(systemName: "house")!),
                createNavController(for: WishlistViewController(), title: NSLocalizedString("Wishlist", comment: ""), image: UIImage(systemName: "person")!),
                createNavController(for: ViewController(), title: NSLocalizedString("Purchased", comment: ""), image: UIImage(systemName: "person")!),
-               createNavController(for: ViewController(), title: NSLocalizedString("Account", comment: ""), image: UIImage(systemName: "person")!)
+               createNavController(for: AccountViewController(), title: NSLocalizedString("Account", comment: ""), image: UIImage(systemName: "person")!)
            ]
        }
     
@@ -32,7 +32,7 @@ class TabBar: UITabBarController {
            let navController = UINavigationController(rootViewController: rootViewController)
            navController.tabBarItem.title = title
            navController.tabBarItem.image = image
-           navController.navigationBar.prefersLargeTitles = true
+           navController.navigationBar.prefersLargeTitles = false
            rootViewController.navigationItem.title = title
            return navController
        }
